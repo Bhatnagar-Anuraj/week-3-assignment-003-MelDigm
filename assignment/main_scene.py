5ccc6d4d87eb7320e7573a6871f9b28d58d3d97e
MelDigm <ml3849@drexel.edu>
2026-05-01 19:36:00 -0400

    Fix variable assignment for create_func and update usage

--- assignment/main_scene.py
+++ assignment/main_scene.py
@@ -42,8 +42,7 @@ create_fence(-6, -3)
 
 pole_node, lamp_node = create_lamp_post(8, 2)
 create_lamp_post(8, 2)
-create_func = place_in_circle
-ree, count=8, radius=7) # ring of trees
+create_func, create_lamp_post = place_in_circle
 place_in_circle(create_lamppost, count=6, radius=5) # ring of lampposts
 
 # ---------------------------------------------------------------------------
