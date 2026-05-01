38dfb8bd7483d23b6f1663be15a1ed7ce3dfc1d7
MelDigm <ml3849@drexel.edu>
2026-05-01 18:52:48 -0400

    Refactor scene functions and remove duplicates
    Refactor scene functions to remove duplicate code and fix variable usage.

--- assignment/scene_functions.py
+++ assignment/scene_functions.py
@@ -24,50 +24,8 @@ GRADING CRITERIA:
 
 import maya.cmds as cmds
 import math
-
-def create_building(width=4, height=8, depth=4, position=(0, 0, 0)):
-    """Create a rectangular building at (x, z)."""
-    building = cmds.polyCube(width=width, height=height, depth=depth)[0]
-    cmds.move(x, height / 2.0, z, building)
-    return building
-building = create_building(-10, 8)
-
-def create_tree(trunk_radius=0.3, trunk_height=3, canopy_radius=2,
-                position=(0, 0, 0)):
-    """Create a simple tree at the given X, Z position."""
-    trunk_radius = 0.3
-    trunk = cmds.polyCylinder(radius=trunk_radius, height=trunk_height)[0]
-    cmds.move(x, trunk_height / 2.0, z, trunk)
-    canopy = cmds.polySphere(radius=canopy_radius)[0]
-    canopy_y = trunk_height + canopy_radius * 0.6
-    cmds.move(x, canopy_y, z, canopy)
-    return trunk, canopy
-trunk, canopy = create_tree(6, -3)
-
-
-def create_fence(length=10, height=1.5, post_count=6, position=(0, 0, 0)):
-    """Create a simple fence made of posts and rails. """
-    post = cmds.polyCylinder(radius=0.1, height=height)[0]
-    cmds.move(x, height / 2.0, z, post)
-    post2 = cmds.polyCylinder(radius=0.1, height=height)[0]
-    rail = cmds.polyCube(width=height, height=.2, depth=,2)[0]
-    cmds.move(x, height-height/2 + 0.25, z, rail)
-    cmds.move(x + height, height / 2.0, z, post2)
-    return post, post2, rail
-post, post2, rail = create_lamppost(-6, -3)
-
-
-def create_lamp_post(pole_height=5, light_radius=0.5, position=(0, 0, 0)):
-    """Create a street lamp using a cylinder pole and a sphere light."""
-    pole = cmds.polyCylinder(radius=0.1, height=height)[0]
-    cmds.move(x, height / 2.0, z, pole)
-    lamp = cmds.polySphere(radius=0.25)[0]
-    cmds.move(x, height + 0.25, z, lamp)
-    return pole, lamp
-pole_node, lamp_node = create_lamppost(6, -3)
-
-
-
+center_x = 0
+center_z = 0
 def place_in_circle(create_func, count=8, radius=10, center=(0, 0, 0),
                      **kwargs):
     """Place objects created by 'create_func' in a circular arrangement."""
@@ -79,4 +37,39 @@ def place_in_circle(create_func, count=8, radius=10, center=(0, 0, 0),
         result = create_func(x, z)
         results.append(result)
     return results
-pole_node, lamp_node = create_lamppost(6, -3)
+
+def create_building(width=4, height=8, depth=4, position=(0, 0, 0)):
+    """Create a rectangular building at (x, z)."""
+    building = cmds.polyCube(width=width, height=height, depth=depth)[0]
+    cmds.move(x, height / 2.0, z, building)
+    return building
+
+def create_tree(trunk_radius=0.3, trunk_height=3, canopy_radius=2,
+                position=(0, 0, 0)):
+    """Create a simple tree at the given X, Z position."""
+    trunk_radius = 0.3
+    trunk = cmds.polyCylinder(radius=trunk_radius, height=trunk_height)[0]
+    cmds.move(x, trunk_height / 2.0, z, trunk)
+    canopy = cmds.polySphere(radius=canopy_radius)[0]
+    canopy_y = trunk_height + canopy_radius * 0.6
+    cmds.move(x, canopy_y, z, canopy)
+    return trunk, canopy
+
+def create_fence(length=10, height=1.5, post_count=6, position=(0, 0, 0)):
+    """Create a simple fence made of posts and rails. """
+    post = cmds.polyCylinder(radius=0.1, height=height)[0]
+    cmds.move(x, height / 2.0, z, post)
+    post2 = cmds.polyCylinder(radius=0.1, height=height)[0]
+    rail = cmds.polyCube(width=height, height=.2, depth=,2)[0]
+    cmds.move(x, height-height/2 + 0.25, z, rail)
+    cmds.move(x + height, height / 2.0, z, post2)
+    return post, post2, rail
+
+def create_lamp_post(pole_height=5, light_radius=0.5, position=(0, 0, 0)):
+    """Create a street lamp using a cylinder pole and a sphere light."""
+    pole = cmds.polyCylinder(radius=0.1, height=height)[0]
+    cmds.move(x, height / 2.0, z, pole)
+    lamp = cmds.polySphere(radius=0.25)[0]
+    cmds.move(x, height + 0.25, z, lamp)
+    return pole, lamp
+
