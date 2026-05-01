90676e565057184a3fd6c2f666f6148303f82e7b
MelDigm <ml3849@drexel.edu>
2026-05-01 19:10:16 -0400

    Clean up variable assignments in main_scene.py
    Removed unused variables from place_in_circle calls.

--- assignment/main_scene.py
+++ assignment/main_scene.py
@@ -40,11 +40,7 @@ post, post2, rail = create_fence(-6, -3)
 
 pole_node, lamp_node = create_lamp_post(8, 2)
 
-create_tree, results = place_in_circle(0, 0)
-
-create_lamp_post, results = place_in_circle(0, 0)
-
-create_fence, results = place_in_circle(0, 0)
+results = place_in_circle(0, 0)
 
 # ---------------------------------------------------------------------------
 # Final viewport framing (do not remove).
