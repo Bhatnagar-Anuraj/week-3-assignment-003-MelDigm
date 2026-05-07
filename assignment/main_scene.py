57919cb8f35ec464a0252e4264bea12eecc3647b
MelDigm <ml3849@drexel.edu>
2026-05-07 12:34:43 -0400

    Update main_scene.py

--- assignment/main_scene.py
+++ assignment/main_scene.py
@@ -33,13 +33,9 @@ cmds.file(new=True, force=True)
 ground = cmds.polyPlane(name="ground", width=60, height=60,
                         subdivisionsX=1, subdivisionsY=1)[0]
 building = create_building(10, 0)
-create_building(10, 0)
 trunk, canopy = create_tree(3, -5)
-create_tree(3, -5)
 pole, lamp = create_fence(-6, -3)
-create_fence(-6, -3)
 pole, lamp = create_lamp_post(8, 2)
-create_lamp_post(8, 2)
 place_in_circle(create_lamppost, count=6, radius=5) # ring of lampposts
 
 # ---------------------------------------------------------------------------
